5f4c811e48e73a2e80f1c3948861932f609e6cb4
Alexey-Kleschov <iron-goga@yandex.ru>
2020-02-07 22:09:20 +0300

    babel, webpack

--- src/main.js
+++ src/main.js
@@ -1,9 +1,25 @@
-class App {
-    run (){
-        const name = 'World';
-        console.log(`Hello ${name}`);
-    }
-}
-const app1 = new App();
-app1.run();
+//JS
 
+import 'core-js';
+
+class App {
+   
+        run = async (name = 'world') => {
+            console.log(`Hello ${name}`);
+            console.log([1, 2, [2, 3]].flat());
+            
+        }
+    
+}
+const app = new App();
+app.run()
+    .then( () => console.log('Done'))
+    .catch( err => console.log(err));
+
+//JSX
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const AppReact = () => <p>Hello world</p>;
+ReactDOM.render(<App />, document.getElementById('root'));
