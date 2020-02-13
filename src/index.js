9dbd6e6a4d7fbc6070f8a70628812b2c4bcda75f
Alexey-Kleschov <iron-goga@yandex.ru>
2020-02-13 20:33:50 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import Log from './log';
 import Calc from './calc';
+import img from './ava_2.jpg';
 
 const log  = new Log();
 const calc = new Calc();
 
 log.log(calc.add(1, 2, 3));
+
+const el = document.createElement('img');
+el.src = img;
+document.body.appendChild(el);
+
+/////////////////////////////////////////////
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './main.css';
+
+const App = () => <p>Hello World!</p>;
+ReactDOM.render(<App />, document.getElementById('root'));
